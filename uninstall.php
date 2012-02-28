aa15a254dfd4c88d47f75e289509c288b3b79603
vicchi <gary@vicchi.org>
2012-02-27 18:03:45 -0800

    Migrate from $wpdb->get_results(…) to use WP_User_Query

--- uninstall.php
+++ uninstall.php
@@ -1,14 +1,14 @@
 <?php
 
 if (defined('WP_UNINSTALL_PLUGIN')) {
-	global $wpdb;
+	$fields = array (0 => 'ID');
+	$search = new WP_User_Query (array ('fields' => $fields));
+	$users = $search->get_results ();
 	
 	// Remove the general WP Biographia options
 	delete_option ('wp_biographia_settings');
 	
 	// Remove the extended user profile metadata added by WP Biographia
-	$users = $wpdb->get_results ("SELECT ID from $wpdb->users ORDER BY ID");
-
 	foreach ($users as $user) {
 		delete_user_meta ($user->ID, 'wp_biographia_suppress_posts');
 		delete_user_meta ($user->ID, 'wp_biographia_suppress_pages');
