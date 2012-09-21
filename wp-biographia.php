2bde502b345c150a380fbdb14766da3a3efc0aa2
vicchi <gary@vicchi.org>
2012-09-21 08:56:37 +0100

    Add wpb_get_biography_box and wpb_the_biography_box template tag functions.

--- wp-biographia.php
+++ wp-biographia.php
@@ -3950,6 +3950,21 @@ class WP_Biographia extends WP_PluginBase {
 	
 }
 
+if (!function_exists ('wpb_get_biography_box')) {
+	function wpb_get_biography_box ($mode='raw', $user=NULL, $prefix=NULL, $name=NULL, $role=NULL, $order='account-name') {
+		$instance = WP_Biographia::get_instance ();
+		$ret = $instance->biography_box ($mode, $user, $prefix, $name, $role, $order);
+		$content = $ret['content'];
+		return implode ('', $content);
+	}	
+}
+
+if (!function_exists ('wpb_the_biography_box')) {
+	function wpb_the_biography_box ($mode='raw', $user=NULL, $prefix=NULL, $name=NULL, $role=NULL, $order='account-name') {
+		echo wpb_get_biography_box ($mode, $user, $prefix, $name, $role, $order);
+	}
+}
+
 WP_Biographia::get_instance ();
 
 ?>
